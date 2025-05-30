b81ed3f08a6c22b9e11dc38ffac14510a87b09a5
Harsh Rawat <harshrawat.dev@gmail.com>
2025-05-30 22:59:42 +0530

    first commit

---
+++ my-animation-script.js
@@ -0,0 +1,577 @@
+// import { roll, lenisInit } from '../utils.js';
+// import { liveReload } from '../liveReload.js';
+
+const header = document.getElementById('header');
+let lastScrollPosition = 0;
+let delta = 50; // Minimum scroll distance before toggling header
+let ticking = false;
+let isMenuOpen = false;
+
+function handleScroll() {
+  const currentScrollPosition = window.scrollY;
+
+  if (Math.abs(currentScrollPosition - lastScrollPosition) > delta) {
+    if (currentScrollPosition > lastScrollPosition) {
+      // Scrolling down
+      header.classList.add('hidden');
+    } else {
+      // Scrolling up
+      header.classList.remove('hidden');
+    }
+    lastScrollPosition = currentScrollPosition;
+  }
+
+  ticking = false;
+}
+export function lenisInit() {
+  // Initialize a new Lenis instance for smooth scrolling
+  const lenis = new Lenis();
+
+  // Synchronize Lenis scrolling with GSAP's ScrollTrigger plugin
+  lenis.on('scroll', ScrollTrigger.update);
+
+  // Add Lenis's requestAnimationFrame (raf) method to GSAP's ticker
+  // This ensures Lenis's smooth scroll animation updates on each GSAP tick
+  gsap.ticker.add((time) => {
+    lenis.raf(time * 1000); // Convert time from seconds to milliseconds
+  });
+
+  // Disable lag smoothing in GSAP to prevent any delay in scroll animations
+  gsap.ticker.lagSmoothing(0);
+
+  return lenis;
+}
+const lenis = lenisInit();
+
+// var scriptLocation = document.currentScript.src;
+// console.log("scriptLocation", scriptLocation);
+
+window.addEventListener('scroll', () => {
+  if (!ticking) {
+    window.requestAnimationFrame(handleScroll);
+    ticking = true;
+  }
+});
+
+(function loaderAnimatiion() {
+  const loaderTl = gsap.timeline({
+    defaults: {
+      duration: 3,
+      ease: 'power4.inOut',
+    },
+  });
+
+  loaderTl
+    .to('.loader_center_line', {
+      height: '0%',
+      duration: 2,
+    })
+    .to(
+      '.loader_line',
+      {
+        height: '0%',
+        duration: 2,
+      },
+      '<'
+    );
+
+  const loader_circle = document.querySelector('.loader_circle');
+  const length = loader_circle.getTotalLength();
+
+  // Set the stroke dash values
+  loader_circle.style.strokeDasharray = length;
+  loader_circle.style.strokeDashoffset = 0;
+
+  loaderTl
+    .to(
+      '.loader_svg',
+      {
+        opacity: '0',
+        delay: 1,
+        duration: 1.3,
+        // duration: 5,
+      },
+      '<'
+    )
+    .to(
+      '.loader_circle',
+      {
+        strokeDashoffset: length,
+        duration: 1.3,
+        // duration: 5,
+      },
+      '<-0.4'
+    );
+
+  loaderTl
+    .to(
+      '.loader_bg_left',
+      {
+        x: '-101%',
+        duration: 2,
+      },
+      '=-0.4'
+    )
+    .to(
+      '.loader_bg_right',
+      {
+        x: '101%',
+        duration: 2,
+      },
+      '<'
+    );
+
+  const hero_svg_path = document.querySelectorAll('.hero_svg path');
+
+  hero_svg_path.forEach((path) => {
+    const length = path.getTotalLength();
+    path.style.strokeDasharray = length;
+    path.style.strokeDashoffset = length;
+  });
+  loaderTl
+    .to(
+      hero_svg_path,
+      {
+        strokeDashoffset: 0,
+        stagger: 0.1,
+      },
+      '=-1.5'
+    )
+    .to(
+      hero_svg_path,
+      {
+        fill: 'rgb(251, 240, 218)',
+        duration: 2,
+      },
+      '<=+1.5'
+    );
+
+  loaderTl
+    .from(
+      '.header_border',
+      {
+        width: 0,
+        duration: 1.5,
+      },
+      '<'
+    )
+    .from(
+      '[nav-reveal]',
+      {
+        y: 30,
+        opacity: 0,
+        stagger: 0.1,
+        duration: 2,
+      },
+      '<'
+    );
+
+  new SplitText('#hero-paragraph', { type: 'lines' });
+  new SplitText('#hero-paragraph', { type: 'lines', linesClass: 'line' });
+
+  loaderTl
+    .from(
+      '#hero-paragraph .line > div',
+      {
+        y: 30,
+        opacity: 0,
+        stagger: 0.1,
+        duration: 2,
+      },
+      '<'
+    )
+    .from(
+      '.hero-svg-small',
+      {
+        opacity: 0,
+        duration: 1.5,
+      },
+      '<'
+    )
+    .from('#scroll-text', { y: 30, opacity: 0, duration: 1.5 }, '<+1');
+})();
+
+const menu_open_tl = gsap.timeline({
+  paused: true,
+  defaults: {
+    duration: 2,
+    ease: 'power4.inOut',
+  },
+});
+menu_open_tl
+  .to('.navigation_left', {
+    x: '0%',
+    duration: 1.5,
+  })
+  .to(
+    '.navigation_right',
+    {
+      x: '0%',
+      duration: 1.5,
+    },
+    '<'
+  )
+  .from(
+    '.nav_border',
+    {
+      width: '0%',
+      duration: 1.5,
+      stagger: 0.05,
+    },
+    '<+0.5'
+  )
+  .from(
+    '.nav_border_left',
+    {
+      width: '0%',
+      duration: 1.5,
+      stagger: 0.05,
+    },
+    '<'
+  )
+  .from(
+    '[nav_animate]',
+    {
+      y: 50,
+
+      duration: 1.5,
+      stagger: 0.05,
+    },
+    '<'
+  )
+  .from(
+    '[nav_animate_left]',
+    {
+      y: 50,
+
+      duration: 1.5,
+      stagger: 0.05,
+    },
+    '<'
+  )
+  .from(
+    '[nav_animate_fade]',
+    {
+      opacity: 0,
+      duration: 1.5,
+      stagger: 0.05,
+    },
+    '<+0.5'
+  );
+
+const menu_close_tl = gsap.timeline({
+  paused: true,
+  defaults: {
+    duration: 2,
+    ease: 'power4.inOut',
+  },
+});
+menu_close_tl
+  .to('.navigation_left', {
+    x: '-101%',
+    duration: 1.5,
+  })
+  .to(
+    '.navigation_right',
+    {
+      x: '101%',
+      duration: 1.5,
+    },
+    '<'
+  );
+
+const menu_trigger = document.querySelector('.menu_trigger');
+const menu_close = document.querySelector('.menu_close');
+
+menu_trigger.addEventListener('click', () => {
+  menu_open_tl.restart();
+  isMenuOpen = true;
+});
+
+menu_close.addEventListener('click', () => {
+  menu_close_tl.restart();
+  isMenuOpen = false;
+});
+
+document.querySelectorAll('[parallax-image]').forEach((image) => {
+  //wrap the image with a div
+  const wrapper = document.createElement('div');
+  wrapper.classList.add('parallax-image-wrapper');
+  wrapper.style.overflow = 'hidden';
+  image.parentNode.insertBefore(wrapper, image);
+  wrapper.appendChild(image);
+
+  const parallaxtl = gsap.timeline({
+    scrollTrigger: {
+      trigger: wrapper,
+      start: 'top bottom',
+      end: 'bottom top',
+      scrub: true,
+      // markers: true,
+    },
+  });
+  const PARALLAXAMOUNT = 140;
+  const imageHeight = image.clientHeight;
+  wrapper.style.height = `${imageHeight}px`;
+  image.style.height = `${imageHeight + PARALLAXAMOUNT}px`;
+
+  parallaxtl.fromTo(
+    image,
+    {
+      y: 0,
+    },
+    {
+      y: -PARALLAXAMOUNT,
+    }
+  );
+
+  if (image.hasAttribute('not-reveal')) {
+    return;
+  }
+
+  gsap.fromTo(
+    wrapper,
+    {
+      clipPath: 'polygon(0 0, 0 0, 0 100%, 0% 100%)',
+    },
+    {
+      clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0% 100%)',
+      duration: 1.5,
+      delay: 0.5,
+      ease: 'power3.inOut',
+      scrollTrigger: {
+        trigger: wrapper,
+        start: 'top bottom',
+        end: 'bottom top',
+      },
+    }
+  );
+});
+
+document.querySelectorAll('[para-reveal]').forEach((text) => {
+  new SplitText(text, { type: 'lines' });
+  new SplitText(text, { type: 'lines', linesClass: 'line' });
+
+  const tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: text,
+      start: 'top bottom',
+      end: 'bottom top',
+    },
+  });
+  tl.fromTo(
+    text.querySelectorAll('.line > div'),
+    {
+      y: '140%',
+    },
+    {
+      y: 0,
+
+      stagger: 0.1,
+      duration: 1.5,
+      ease: 'power4.inOut',
+    }
+  );
+});
+
+document.querySelectorAll('[basic-reveal]').forEach((element) => {
+  // gsap.from(element, {
+  //   scrollTrigger: {
+  //     trigger: element,
+  //     start: "top bottom",
+  //     end: "bottom top",
+  //   },
+  //   y: "150%",
+  //   duration: 1.5,
+  //   ease: "power4.inOut",
+  // });
+
+  gsap.set(element, {
+    y: '150%',
+  });
+});
+
+ScrollTrigger.batch('[basic-reveal]', {
+  start: 'top bottom',
+  end: 'bottom top',
+  // markers: true,
+  onEnter: (elements, triggers) => {
+    gsap.to(elements, {
+      y: 0,
+      stagger: 0.15,
+      duration: 1.5,
+      ease: 'power3.inOut',
+    });
+  },
+});
+
+document.querySelectorAll('[fade-in]').forEach((element) => {
+  gsap.set(element, {
+    opacity: 0,
+  });
+});
+
+ScrollTrigger.batch('[fade-in]', {
+  start: 'top bottom',
+  end: 'bottom top',
+  // markers: true,
+  onEnter: (elements, triggers) => {
+    gsap.to(elements, {
+      opacity: 1,
+      stagger: 0.1,
+      duration: 3,
+      ease: 'power3.inOut',
+    });
+  },
+});
+
+document.querySelectorAll('[border-reveal]').forEach((element) => {
+  gsap.from(element, {
+    scrollTrigger: {
+      trigger: element,
+      start: 'top bottom',
+      end: 'bottom top',
+    },
+    width: '0%',
+    duration: 1.5,
+    ease: 'power4.inOut',
+  });
+});
+
+document.querySelectorAll('.footer_link_content').forEach((element) => {
+  const tl = gsap.timeline({
+    paused: true,
+    defaults: {
+      duration: 0.5,
+      ease: 'none',
+    },
+  });
+  tl.fromTo(
+    element.querySelector('.footer_link_border'),
+    {
+      left: '0%',
+      right: '100%',
+    },
+    {
+      left: '0%',
+      right: '0%',
+    }
+  ).to(element.querySelector('.footer_link_border'), {
+    right: '0%',
+    left: '100%',
+  });
+
+  let tween = null;
+
+  // Kill any running tween
+  const stopTween = () => {
+    if (tween) {
+      tween.kill();
+      tween = null;
+    }
+  };
+
+  // Reset timeline when it's done
+  tl.eventCallback('onComplete', () => {
+    tl.pause(0); // Go back to start, paused
+  });
+
+  element.addEventListener('mouseenter', () => {
+    // play the animation until 50%
+
+    stopTween();
+
+    tween = gsap.to(tl, {
+      progress: 0.5,
+      duration: Math.abs(tl.progress() - 0.5) * tl.duration(),
+      ease: 'power3.out',
+    });
+
+    gsap.to(element.querySelector('.footer_link_shadow'), {
+      width: '100%',
+      duration: 0.5,
+      ease: 'power3.inOut',
+    });
+  });
+  element.addEventListener('mouseleave', () => {
+    stopTween();
+
+    tween = gsap.to(tl, {
+      progress: 1,
+      duration: (1 - tl.progress()) * tl.duration(),
+      ease: 'power3.inOut',
+    });
+
+    // Optional: reset timeline after complete if you want repeat behavior
+    tween.then(() => {
+      tl.pause(0);
+    });
+
+    gsap.to(element.querySelector('.footer_link_shadow'), {
+      width: '0%',
+      duration: 0.5,
+      ease: 'power3.inOut',
+    });
+  });
+});
+
+const processBlocks = document.querySelectorAll('.process-block');
+
+processBlocks.forEach((element) => {
+  element.isOpen = false;
+
+  element.addEventListener('mouseenter', () => {
+    gsap.to(element.querySelector('.bg-off-brown'), {
+      top: '0%',
+      duration: 0.5,
+      ease: 'power3.inOut',
+    });
+  });
+
+  element.addEventListener('click', () => {
+    // all other elements should be closed
+    processBlocks.forEach((processBlock) => {
+      if (processBlock !== element) {
+        processBlock.isOpen = false;
+        toggleProcess(processBlock, true);
+      }
+    });
+
+    toggleProcess(element, element.isOpen);
+    element.isOpen = !element.isOpen;
+  });
+
+  element.addEventListener('mouseleave', () => {
+    gsap.to(element.querySelector('.bg-off-brown'), {
+      top: '100%',
+      duration: 0.5,
+      ease: 'power3.inOut',
+    });
+  });
+});
+
+function toggleProcess(element, isOpen) {
+  gsap.to(element.querySelector('.process-answer'), {
+    height: isOpen ? '0px' : 'auto',
+    duration: 0.5,
+    ease: 'power3.inOut',
+  });
+  gsap.to(element.querySelectorAll('.process-btn'), {
+    y: isOpen ? '0%' : '-100%',
+    duration: 0.5,
+    ease: 'power3.inOut',
+  });
+
+  if (!isOpen) {
+    gsap.from(element.querySelectorAll('[process-reveal]'), {
+      y: '50px',
+      stagger: 0.01,
+      duration: 1,
+      ease: 'power3.inOut',
+    });
+  }
+}
+
+// console.log("From how it why");
+// roll("[roll]", 80);
+// liveReload();
---
+++ utils.js
@@ -0,0 +1,91 @@
+export const getCurrentTime = () => {
+  return new Date().toISOString();
+};
+
+export function debounce(func, delay) {
+  let timeoutId;
+  return function (...args) {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => func.apply(this, args), delay);
+  };
+}
+
+export function scrollResotration() {
+  window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+  };
+
+  if ('scrollRestoration' in history) {
+    history.scrollRestoration = 'manual';
+  }
+}
+
+export function lenisInit() {
+  // Initialize a new Lenis instance for smooth scrolling
+  const lenis = new Lenis();
+
+  // Synchronize Lenis scrolling with GSAP's ScrollTrigger plugin
+  lenis.on('scroll', ScrollTrigger.update);
+
+  // Add Lenis's requestAnimationFrame (raf) method to GSAP's ticker
+  // This ensures Lenis's smooth scroll animation updates on each GSAP tick
+  gsap.ticker.add((time) => {
+    lenis.raf(time * 1000); // Convert time from seconds to milliseconds
+  });
+
+  // Disable lag smoothing in GSAP to prevent any delay in scroll animations
+  gsap.ticker.lagSmoothing(0);
+
+  return lenis;
+}
+
+export function getDevices() {
+  return {
+    isDesktop: window.innerWidth > 991,
+    isMobile: window.innerWidth < 991,
+  };
+}
+
+export function roll(
+  targets,
+  pixelPerSecond = 1000,
+  isReverse = false,
+  gsapVars = {}
+) {
+  gsapVars.ease || (gsapVars.ease = 'none');
+  const rollAnims = [];
+  const elements = gsap.utils.toArray(targets);
+  elements.forEach((el, i) => {
+    const pxSeconds = el.offsetWidth / pixelPerSecond;
+
+    // console.log(pxSeconds);
+    const clone = el.cloneNode(true);
+    el.parentNode?.appendChild(clone);
+    const rollAnim = gsap.to([el, clone], {
+      xPercent: (isReverse = false ? 100 : -100),
+      duration: pxSeconds,
+      repeat: -1,
+      onReverseComplete() {
+        this.totalTime(this.rawTime() + this.duration() * 10); // otherwise when the playhead gets back to the beginning, it'd stop. So push the playhead forward 10 iterations (it could be any number)
+      },
+      ...gsapVars,
+    });
+    rollAnims.push(rollAnim);
+  });
+
+  return rollAnims;
+}
+
+export function convertVhToFixedHeight() {
+  // Select all sections with vh-based heights (adjust selector as needed)
+  const sections = document.querySelectorAll('[data-vh-section]');
+
+  sections.forEach((section) => {
+    // Get computed height of the section
+    const computedHeight = section.offsetHeight;
+
+    // Set the fixed height in pixels
+    section.style.height = `${computedHeight}px`;
+    section.style.minHeight = 'unset'; // To override any min-height
+  });
+}
